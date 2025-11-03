195750062a6134e43bab7eda4560e136a793bb46
CarlangasBeja <carlangasbeja@gmail.com>
2025-11-03 11:37:00 -0500

    Se creo el metodo de las rutas para poder trabajar con el user

--- backend/src/presentation/routes/user.routes.js
+++ backend/src/presentation/routes/user.routes.js
@@ -1,1 +1,14 @@
 //se creo la arquitectura de rutas para el uso de user
+import { Router } from "express";
+import { UserController } from "../../infrastructure/controllers/user.controller.js";
+
+const router = Router();
+
+router.get("/", UserController.getAll);
+router.get("/:id", UserController.getById);
+router.post("/", UserController.create);
+router.put("/:id", UserController.update);
+router.delete("/:id", UserController.delete);
+
+export default router;
+//Se creo el codigo de rutas para user
