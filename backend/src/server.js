e36ff4adbbb66a1723b2e19da9713b4457f37e7c
CarlangasBeja <carlangasbeja@gmail.com>
2025-11-03 11:39:24 -0500

    se creo el codigo de la clase server para uso del user[C

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,13 @@
 //se creo la clase de server para lka funcion principal de user
+import express from "express";
+import cors from "cors";
+import userRoutes from "./presentation/routes/user.routes.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`✅ Servidor User corriendo en puerto ${PORT}`));
