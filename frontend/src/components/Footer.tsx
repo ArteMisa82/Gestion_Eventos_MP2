13a074b0ef794615ff3a8b3dadf584d52596d35f
Evelyn Cardenas <evelyncardenas14.07@hotmail.com>
2025-11-26 21:48:23 -0500

    Se finalizo el cambio de rutas y tambien se modifico el footer con los redireccionamientos necesarios

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -6,38 +6,31 @@ export default function Footer() {
       <div className={styles.top}>
         <div className={styles.brand}>UTA · Gestión de Eventos</div>
         <div className={styles.socials}>
-          <a href="#" aria-label="Facebook">Facebook</a>
-          <a href="#" aria-label="Instagram">Instagram</a>
-          <a href="#" aria-label="Google">Google</a>
-          <a href="#" aria-label="LinkedIn">LinkedIn</a>
+          <a href="https://www.facebook.com/UniversidadTecnicadeAmbatoOficial/?locale=es_LA" aria-label="Facebook">Facebook</a>
+          <a href="https://www.instagram.com/utecnicaambato/" aria-label="Instagram">Instagram</a>
         </div>
       </div>
 
       <div className={styles.grid}>
         <div>
-          <h4>Useful Links</h4>
+          <h4>Enlaces Útiles</h4>
           <ul>
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Eventos</a></li>
-            <li><a href="#">Certificados</a></li>
+            <li><a href="http://localhost:3000/nosotros">Nosotros</a></li>
+            <li><a href="http://localhost:3000/cursos">Eventos</a></li>
           </ul>
         </div>
 
         <div>
-          <h4>Support</h4>
+          <h4>Soporte</h4>
           <ul>
-            <li><a href="#">Preguntas frecuentes</a></li>
-            <li><a href="#">Ayuda</a></li>
-            <li><a href="#">Soporte técnico</a></li>
+            <li><a href="#">Formulario de cambios</a></li>
           </ul>
         </div>
 
         <div>
-          <h4>Contact Us</h4>
+          <h4>Contáctanos</h4>
           <ul>
-            <li>Av. Universidad, Ambato</li>
-            <li>soporte@uta.edu.ec</li>
-            <li>+593 99 999 9999</li>
+            <li>Avenida los chásquis, y, Ambato</li>
           </ul>
         </div>
       </div>
